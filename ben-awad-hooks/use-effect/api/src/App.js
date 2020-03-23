fb2e18f6d7b9769e7d3820a1e4da93466a11dc1f
krynskibartosz <krynskibartosz08@gmail.com>
2020-03-23 12:28:26 +0100

    Change les donnée de l'api en fonction du nombre choisis

--- ben-awad-hooks/use-effect/api/src/App.js
+++ ben-awad-hooks/use-effect/api/src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useForm  from "./useForm";
 import {useFetch} from './useFect';
 // import Hello from "./Hello"
-
 
 const App = () => {
   //! On utilise le useForm hook qui vien du composant useForm.jsx
   // ainsi on peut récuperer toute la logique de la récupération des valeurs des inputs dans un seul hooks
   const [values, handleChange] = useForm({email: '', password: '', firstName:''});
-  const {data, loading} = useFetch("http://numbersapi.com/43/trivia")
+  const [count, setCount] = useState(0);
+  const {data, loading} = useFetch(`http://numbersapi.com/${count}/trivia`);
 
   return (
     <main>
     <blockquote>
       {loading ? 'loading...' : data}
     </blockquote>
+    <p>count : {count}</p>
+    <button onClick={() => setCount(c => c + 1)}>Incrémente</button>
     <>
       <input name='email' value={values.email} onChange={handleChange}/>
       <input name='firstName' value={values.firstName} onChange={handleChange}/>
--- ben-awad-hooks/use-effect/api/src/useFect.jsx
+++ ben-awad-hooks/use-effect/api/src/useFect.jsx
@@ -3,7 +3,7 @@ import {useEffect, useState } from "react";
 export const useFetch = url => {
     const [state, setState] = useState({ data: null, loading: true});
     useEffect(() => {
-        setState({data: null, loading: true});
+        setState(state => ({data : state.data, loading: true}));
         //? Ici ont est obliger d'utiliser une IIFE(Immediately Invoked Function Expression)
         //! C/ nous somme dans un useEffect Hooks.
         (async () => {
@@ -11,10 +11,9 @@ export const useFetch = url => {
             let value = await res.text();
             setState({ data: value, loading: false });
         })();
-        }, [url]);
+        }, [url, setState]);
     return state;
 }
-
 
 
 // On aurai pu le faire avec des callbaks comme sa
