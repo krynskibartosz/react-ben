d63963db512022880e5dbaeba6947786d31400ac
krynskibartosz <krynskibartosz08@gmail.com>
2020-03-23 23:28:47 +0100

    Manipulation d'une api

--- ben-awad-hooks/use-effect/api/src/App.js
+++ ben-awad-hooks/use-effect/api/src/App.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useForm  from "./useForm";
-import {useFetch} from './useFect';
+import {useFetch} from './useFetch';
 // import Hello from "./Hello"
 
 const App = () => {
   //! On utilise le useForm hook qui vien du composant useForm.jsx
   // ainsi on peut récuperer toute la logique de la récupération des valeurs des inputs dans un seul hooks
   const [values, handleChange] = useForm({email: '', password: '', firstName:''});
-  const [count, setCount] = useState(0);
-  const {data, loading} = useFetch(`http://numbersapi.com/${count}/trivia`);
+  const [count, setCount] = useState(() => JSON.parse(localStorage.getItem("count")));
+  const {data, loading} = useFetch(`http://numbersapi.com/${count ? count : 0}/trivia`);
+
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count));
+  }, [count]);
 
   return (
     <main>
     <blockquote>
-      {loading ? 'loading...' : data}
+      {loading ? "loading..." : data}
     </blockquote>
     <p>count : {count}</p>
     <button onClick={() => setCount(c => c + 1)}>Incrémente</button>
